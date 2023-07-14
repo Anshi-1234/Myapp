483adca6018496fc01ab3ea090b008ebfa2d0913
Anshika Singla <71587985+Anshi-1234@users.noreply.github.com>
2023-07-14 11:56:11 +0530

    Update app.py

--- app.py
+++ app.py
@@ -33,7 +33,7 @@ def run_app():
     prompt = st.text_area("Enter the existing content:")
     token_count = st.number_input("Enter the desired token count:", min_value=1, step=1)
     source_language = st.selectbox("Enter the source language:",
-    ('en, 'auto', 'ko','es','el','ru))
+    ('en,'ko','es','el','ru))
     temperature = st.slider("Select the temperature:", min_value=0.1, max_value=1.0, step=0.1, value=0.7)
     n_gen = st.number_input("Enter the number of generated content:", min_value=1, step=1)
 
